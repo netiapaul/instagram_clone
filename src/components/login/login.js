fff6e93b59e6e79189caacc037c87be762eafc87
Netia Paul <netiapaul23@gmail.com>
2021-09-15 23:26:24 +0300

    added the first login layout

---
+++ src/components/login/login.js
@@ -0,0 +1,11 @@
+import "./login.css";
+
+const Login = () => {
+  return (
+    <div className="card mx-auto">
+      <div className="card-body">This is some text within a card body.</div>
+    </div>
+  );
+};
+
+export default Login;
