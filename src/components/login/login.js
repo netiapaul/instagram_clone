ba7d09d8bc015acf55ba155c8959a5d962ecdf92
Netia Paul <netiapaul23@gmail.com>
2021-09-16 00:23:34 +0300

    added forms in login page

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,9 +1,88 @@
 import "./login.css";
+import { useFormik } from "formik";
+
+// A custom validation function. This must return an object
+// which keys are symmetrical to our values/initialValues
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
+
+  if (!values.password) {
+    errors.password = "Required";
+  } else if (values.password.length > 15) {
+    errors.password = "Must be 15 characters or less";
+  }
+
+  return errors;
+};
 
 const Login = () => {
+  // Pass the useFormik() hook initial form values, a validate function that will be called when
+  // form values change or fields are blurred, and a submit function that will
+  // be called when the form is submitted
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <div className="card mx-auto">
-      <div className="card-body">This is some text within a card body.</div>
+      <div className="card-body">
+        <form onSubmit={formik.handleSubmit}>
+          <div className="mb-3">
+            {/* <label htmlFor="email">Email Address</label> */}
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="email"
+              className="form-control"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+            />
+
+            {formik.touched.email && formik.errors.email ? (
+              <div className="text-danger">{formik.errors.email}</div>
+            ) : null}
+          </div>
+
+          <div className="mb-3">
+            {/* <label htmlFor="password">Last Name</label> */}
+            <input
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Password"
+              className="form-control"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+            />
+
+            {formik.touched.password && formik.errors.password ? (
+              <div className="text-danger">{formik.errors.password}</div>
+            ) : null}
+          </div>
+
+          <div class="d-grid gap-2 col mx-auto">
+            <button type="submit" className="btn btn-primary">
+              Log In
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
