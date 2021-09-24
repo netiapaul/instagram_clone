5ead76ff62a76d47a7de8cc8644f01e7dbc3b3b7
Netia Paul <netiapaul23@gmail.com>
2021-09-24 09:03:34 +0300

    added the login layout

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,6 +1,6 @@
 import "./login.css";
 import { useFormik } from "formik";
-
+import facebook from "../../assets/images/facebook.png";
 // A custom validation function. This must return an object
 // which keys are symmetrical to our values/initialValues
 const validate = (values) => {
@@ -37,53 +37,81 @@ const Login = () => {
   });
 
   return (
-    <div className="card mx-auto">
-      <div className="card-body">
-        <form onSubmit={formik.handleSubmit}>
-          <div className="mb-3">
-            {/* <label htmlFor="email">Email Address</label> */}
-            <input
-              id="email"
-              name="email"
-              type="email"
-              placeholder="email"
-              className="form-control"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.email}
-            />
+    <>
+      <div className="card mx-auto">
+        <div className="card-body">
+          <form onSubmit={formik.handleSubmit}>
+            <div className="mb-3">
+              {/* <label htmlFor="email">Email Address</label> */}
+              <input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="email"
+                className="form-control"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+              />
 
-            {formik.touched.email && formik.errors.email ? (
-              <div className="text-danger">{formik.errors.email}</div>
-            ) : null}
+              {formik.touched.email && formik.errors.email ? (
+                <div className="text-danger">{formik.errors.email}</div>
+              ) : null}
+            </div>
+
+            <div className="mb-3">
+              {/* <label htmlFor="password">Last Name</label> */}
+              <input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Password"
+                className="form-control"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+              />
+
+              {formik.touched.password && formik.errors.password ? (
+                <div className="text-danger">{formik.errors.password}</div>
+              ) : null}
+            </div>
+
+            <div className="d-grid gap-2 col mx-auto">
+              <button type="submit" className="btn btn-primary">
+                Log In
+              </button>
+            </div>
+          </form>
+
+          <div className="mx-auto my-3">
+            <div className="row">
+              <div className="col">
+                <img
+                  src={facebook}
+                  className="img-fluid"
+                  width="30"
+                  alt="facebook logo"
+                />
+              </div>
+              <div className="col">Login with Facebok</div>
+            </div>
           </div>
 
-          <div className="mb-3">
-            {/* <label htmlFor="password">Last Name</label> */}
-            <input
-              id="password"
-              name="password"
-              type="password"
-              placeholder="Password"
-              className="form-control"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.password}
-            />
-
-            {formik.touched.password && formik.errors.password ? (
-              <div className="text-danger">{formik.errors.password}</div>
-            ) : null}
+          <div className="mx-auto my-3">
+            <div className="row">
+              <div className="col text-center">Forgot password?</div>
+            </div>
           </div>
-
-          <div class="d-grid gap-2 col mx-auto">
-            <button type="submit" className="btn btn-primary">
-              Log In
-            </button>
-          </div>
-        </form>
+        </div>
       </div>
-    </div>
+
+      <div className="card my-3">
+        <div className="card-body text-center">
+          <p>Dont have an account? Sign up</p>
+        </div>
+      </div>
+    </>
   );
 };
 
